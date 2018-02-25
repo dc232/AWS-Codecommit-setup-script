11fcbb88c5455a0760610d3ca857e6f907fb1bad
Danny <danny_chauhan@hotmail.com>
2018-02-25 13:43:23 +0000

    Update Codecommit.sh
    Updated code to allow for different python versions to be entered if the version changes also added variable to allow for easy injection of the AWS programmatic access key

--- Codecommit.sh
+++ Codecommit.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
+##########################
+#GLOBAL VARS
+###########################
+PYTHON_VERSION="2.7.10"
+ACESS_KEY="EXAMPLE"
+
 aws_cli_install () {
-sudo apt-get update
-sudo apt-get install python3.6
+sudo apt update
+sudo apt install python3.6
 mkdir python
 cd python
-wget https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
-tar -zxvf Python-2.7.10.tgz
-sudo apt-get install build-essential checkinstall -y
-sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libgdbm-dev libc6-dev libbz2-dev libsqlite3-dev tk-dev libssl-dev -y
-cd Python-2.7.10
+wget https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tgz
+tar -zxvf Python-$PYTHON_VERSION.tgz
+sudo apt install build-essential checkinstall -y
+sudo apt install libreadline-gplv2-dev libncursesw5-dev libgdbm-dev libc6-dev libbz2-dev libsqlite3-dev tk-dev libssl-dev -y
+cd Python-$PYTHON_VERSION
 sudo ./configure
 sudo make altinstall
 whereis python2.7
 python2.7 --version
-sudo apt-get install python-pip python-dev build-essential -y
+sudo apt install python-pip python-dev build-essential -y
 pip install --upgrade pip
 sudo pip install --upgrade virtualenv
 pip install awscli --upgrade --user
@@ -22,28 +28,28 @@ pip install awscli --upgrade --user
 }
 
 git_install () {
-sudo apt-get install git -y
+sudo apt install git -y
 }
 
 config () {
-cat &lt;&lt; EOF &gt;&gt; config
+cat <<"EOF">> config
 Host git-codecommit.*.amazonaws.com
-User APKAEIBAERJR2EXAMPLE
+User $ACESS_KEY
 IdentityFile ~/.ssh/id_rsa
 EOF
 }
 
 ssh_key_conf () {
-sudo apt-get update &amp;&amp; sudo apt-get upgrade -y
+sudo apt update && sudo apt upgrade -y
 ssh localhost true
 cd .ssh
 ssh-keygen -t rsa -b 4096 -f id_rsa -N ''
 touch ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 cp ~/.ssh/authorized_keys ~/.ssh/authorized_keys_Backup
-cat ~/.ssh/id_rsa.pub &gt;&gt; ~/.ssh/authorized_keys
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 echo 'restarting SSH Deamon'
-sudo service ssh restart
+systemctl restart ssh
 config
 }
 
